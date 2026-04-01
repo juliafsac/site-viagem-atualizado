cdb28ed17f3dcbf3ab0746b04b15090190922b3f
juliafsac <juliafs.ac@gmail.com>
2026-04-01 16:55:13 -0500

    Atualizando arquivo

---
+++ src/app/tourz/tourz.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tourz',
+  imports: [],
+  templateUrl: './tourz.html',
+  styleUrl: './tourz.css',
+})
+export class Tourz {}
